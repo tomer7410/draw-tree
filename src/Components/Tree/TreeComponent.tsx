02e6dea28ce81f219482b3017aeed02597446d9a
Tomerm7410 <rooom784@gmail.com>
2023-02-27 19:06:51 +0200

    draw lines p2

--- src/Components/Tree/TreeComponent.tsx
+++ src/Components/Tree/TreeComponent.tsx
@@ -7,33 +7,51 @@ function radians_to_degrees(radians:number)
   var pi = Math.PI;
   return radians * (180/pi);
 }
-const drawLine = (startedPoint:Point, angle:number , length:number) => {
+const drawLine = (startedPoint:Point, angle:number , length:number,offsetx:number, offsetY:number) => {
     return (
-        <div style ={{width:'1px', height:length, background:'red', position:'absolute', top : startedPoint.y, left : startedPoint.x,transform:`rotate(${angle}deg)`}}>
+        <div style ={{width:'1px', height:length, background:'red', position:'absolute', top : startedPoint.y + offsetY, left : startedPoint.x + offsetx,transform:`rotate(${angle}deg)`}}>
 
         </div>
     )
 }
 const createLines = (father:JSX.Element,children:JSX.Element[],nodeSize:number) =>{
     const offset =  0.5 * nodeSize
-    const fatherPoint : Point = { x:father.props.style.left + offset, y:father.props.style.top + nodeSize }
-    const childrenPoints = children.map((child:JSX.Element) : Point=>({x:child.props.tree.x + offset,y:child.props.tree.y}))
+    const fatherPoint : Point = { x:father.props.style.left + offset, y:father.props.style.top + nodeSize}
+    const childrenPoints = children.map((child:JSX.Element) : Point=>({x:child.props.tree.x + offset   ,y:child.props.tree.y}))
     const nOfChildren = children.length
     let lines = null
     if(nOfChildren == 1){
-        lines =  drawLine(fatherPoint,0,childrenPoints[0].y - fatherPoint.y)
+        lines =  drawLine(fatherPoint,0,childrenPoints[0].y - fatherPoint.y,0,0)
     }
     else{
         
-        const limitIndex = nOfChildren % 2 == 0 ? nOfChildren / 2 : Math.floor((nOfChildren / 2)) + 1
+        const limitIndex =  Math.floor((nOfChildren / 2))
         lines =  childrenPoints.map((childPos:Point,index:number) =>{
             const xDiff = (fatherPoint.x - childPos.x)
             const yDiff = (childPos.y - fatherPoint.y)
-            const d = Math.abs(Math.sqrt(xDiff*xDiff + yDiff*yDiff))
+            const d = Math.abs(Math.floor(Math.sqrt(xDiff*xDiff + yDiff*yDiff)))
+            const r = d / 2
             const x = fatherPoint.x - childPos.x
             const y = childPos.y - fatherPoint.y
-            const angle  = Math.atan2(y,x) * 180 / Math.PI 
-            return (drawLine(fatherPoint,angle,d))
+            const angle  = Math.atan(x/y) * 180 / Math.PI
+            const nitzav =  r * Math.sin(angle)
+            const yeter  = 2 * Math.sqrt(r * r - nitzav * nitzav )
+            const zavit = 90 - ((180 - angle)/2)
+            const nitzav1 = yeter * Math.cos(zavit)
+            const nitzav2 = Math.abs(yeter * Math.sin(zavit))
+            let offsetY = -nitzav2
+            let offsetX = 0
+            if(index<limitIndex){
+                offsetX = nitzav1
+                // offsetY = -nitzav2
+                // fatherPoint.x -= nitzav1
+            }
+            else{
+                offsetX = -nitzav1
+                // offsetY = -nitzav2
+                // fatherPoint.x += nitzav1
+            }
+            return (drawLine(fatherPoint,angle,d,offsetX,offsetY))
             
         })
     }
